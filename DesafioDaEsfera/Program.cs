f735bde34f0a229d3461db014245fb160e1ec050
Victhor <victhor.ribeiro@hotmail.com>
2022-02-26 17:37:14 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioDaEsfera/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class DIO
+{
+    static void Main(string[] args)
+    {
+        double pi = 3.14159;
+
+        int numeroTentativas = 0;
+
+        while(numeroTentativas < 3)
+        {
+            double raio = double.Parse(Console.ReadLine());
+            double volumeEsfera = ((4 / 3) * pi * Math.Pow(raio, 2));
+
+            Console.WriteLine($"VOLUME = {volumeEsfera}");
+            numeroTentativas++;
+        }
+    }
+}
